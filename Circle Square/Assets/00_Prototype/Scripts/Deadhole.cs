69e79e33b3213a9af70a716e3c4fa1bfc8610dc4
LalDi <jungjh0513@naver.com>
2020-09-15 00:03:34 +0900

    Prototype 0.1
    플레이어 이동 구현
    데드홀 구현

---
+++ Circle Square/Assets/00_Prototype/Scripts/Deadhole.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+public class Deadhole : MonoBehaviour
+{
+    public float m_Speed = 1;
+    public GameObject ReButton;
+
+    private void Start()
+    {
+        StartCoroutine(Move());
+    }
+
+    private IEnumerator Move()
+    {
+        transform.DOMoveX(transform.position.x + m_Speed, 1).SetEase(Ease.Linear);
+        yield return new WaitForSeconds(1.0f);
+        StartCoroutine(Move());
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Time.timeScale = 0;
+            ReButton.SetActive(true);
+        }
+    }
+
+    public void ReStart()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("00_Prototype");
+    }
+}
---
+++ Circle Square/Assets/00_Prototype/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Player : MonoBehaviour
+{
+    private bool IsMove = false;
+
+    public float MoveDelay;
+    public float MovePower;
+    public Ease GetEaseMove;
+
+    public float JumpDelay;
+    public Ease GetEaseJump;
+
+    public void Go()
+    {
+        StartCoroutine(Move());
+    }
+
+    private IEnumerator Move()
+    {
+        if (!IsMove)
+        {
+            IsMove = true;
+            transform.DOMoveX(transform.position.x + MovePower, MoveDelay).SetEase(GetEaseMove);
+            yield return new WaitForSeconds(MoveDelay);
+            IsMove = false;
+        }
+        yield return 0;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            transform.DOMoveY(transform.position.y + 3, JumpDelay).SetEase(GetEaseJump);
+        }
+    }
+}
